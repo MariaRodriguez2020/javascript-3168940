bcb332a561291f460b2ca122b45120cdff62440d
MariaRodriguez2020 <mariafernandarodriguez186@gmail.com>
2025-10-28 15:57:33 -0500

    Add files via upload
    avanceeee

--- 02_escenario_interactivo/index.js
+++ 02_escenario_interactivo/index.js
@@ -19,3 +19,28 @@ oso.forEach ( item => {
  
 
 })
+
+// variable 
+
+const escena = document.querySelectorAll(".escena")
+const anterior = document.querySelectorAll(".anterior")
+const siguiente = document.querySelectorAll(".siguiente")
+const miniatura = document.querySelectorAll(".miniatura")
+
+let indice = 0
+
+console.log(escena)
+console.log(siguiente)
+console.log(anterior)
+console.log(miniatura)
+
+//funciones 
+
+function mostrarEscena(1){
+
+    for(let j = 0;j <escenas . length; j++){
+        escenas [j].classList.remove("activa")
+
+        indice = 1
+    }
+}
