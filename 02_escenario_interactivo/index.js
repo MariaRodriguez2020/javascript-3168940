83b2ff21d0c9e69eb396e1efd8223d1f2b1a923f
MariaRodriguez2020 <mariafernandarodriguez186@gmail.com>
2025-11-04 23:17:09 -0500

    Add files via upload
    Escenario, casi terminado

--- 02_escenario_interactivo/index.js
+++ 02_escenario_interactivo/index.js
@@ -1,46 +1,133 @@
-const oso= document.querySelectorAll(".oso")
-const tablero = document.querySelector ("#contador-numero")
-
+const oso = document.querySelectorAll(".oso") //clases
+const tablero = document.querySelector("#contador-numero") //id
 let counter = 0
+
 console.log(oso, tablero)
 
-// funciones 
+// 2. Funciones
 
-oso.forEach ( item => {
-    item.style.filter = "grayscale(1)"
-
-    item.addEventListener("click", ()=> {
+oso.forEach( item => {
+    
+    item.addEventListener ("click", () => {
         item.style.filter = "grayscale(1)"
         item.classList.add("saltar")
         counter++
         tablero.textContent = counter
     })
-
- 
-
 })
 
-// variable 
 
-const escena = document.querySelectorAll(".escena")
-const anterior = document.querySelectorAll(".anterior")
-const siguiente = document.querySelectorAll(".siguiente")
-const miniatura = document.querySelectorAll(".miniatura")
 
-let indice = 0
+// ESCENARIO 2 ======================================================================================================================
 
-console.log(escena)
-console.log(siguiente)
+const personajes = document.querySelectorAll(".personajes") //clases
+const contadorPersonajes = document.querySelector("#contador-personajes") //id
+let counterDos = 0
+
+personajes.forEach(item => {
+
+    item.addEventListener("click", () => {
+        if (item.classList.contains("cambioColor")) return;
+        item.classList.add("cambioColor");
+        setTimeout(() => {
+        item.classList.add("saltar");
+
+            
+            setTimeout(() => {
+                item.style.display = "none";
+            }, 600);
+
+        }, 600); 
+
+        counterDos++;
+        contadorPersonajes.textContent = counterDos;
+
+        contadorPersonajes.classList.add("activo");
+        setTimeout(() => contadorPersonajes.classList.remove("activo"), 300);
+    });
+});
+
+
+// ESCENARIO 3 ======================================================================================================================
+
+const caracol = document.getElementById("caracol");
+const contador = document.getElementById("contador-movimientos"); 
+let clics = 0; 
+
+caracol.addEventListener("click", function() {
+  clics = clics + 1;
+
+  contador.textContent = clics;
+
+  if (clics === 1) {
+    caracol.style.transform = "translateX(160px)";
+  }
+
+  else if (clics === 2) {
+    caracol.style.transform = "translateX(160px) translateY(-220px)";
+  }
+
+  else if (clics === 3) {
+    caracol.style.transform = "translateX(350px) translateY(-220px)";
+  }
+
+  else if (clics === 4) {
+    caracol.style.animation = "saltoCaracol 0.6s ease-in-out infinite";
+    caracol.style.transform = "translateX(350px) translateY(-220px)";
+  }
+});
+
+
+
+// Carrusel  =======================================================================================================================0
+
+//  1.Variables
+ const escenas = document.querySelectorAll(".escena")
+ const anterior = document.querySelector(".anterior")
+ const siguiente = document.querySelector(".siguiente")
+ const miniatura = document.querySelectorAll(".miniatura")
+ let indice = 0
+
+console.log(escenas)
 console.log(anterior)
+console.log(siguiente)
 console.log(miniatura)
 
-//funciones 
+// 2.Funciones
+function mostrarEscena(i){
 
-function mostrarEscena(1){
 
-    for(let j = 0;j <escenas . length; j++){
-        escenas [j].classList.remove("activa")
+    for (let j = 0; j < escenas.length; j++) {
+        escenas[j].classList.remove("activa")
+     }
 
-        indice = 1
+    escenas[i].classList.add("activa")
+
+    indice = i
+ }
+
+
+mostrarEscena(0)
+
+anterior.addEventListener("click", function(){
+    indice = indice - 1
+    if(indice < 0){
+        indice = escenas.length - 1 
     }
-}
+ mostrarEscena(indice)
+})
+
+siguiente.addEventListener("click", function(){
+    indice = indice + 1
+    if(indice >= escenas.length){
+        indice = 0
+    }
+    mostrarEscena(indice)
+})
+
+
+miniatura.forEach(function(miniatura, i) {
+  miniatura.addEventListener('click', function () {
+    mostrarEscena(i);
+  });
+});
