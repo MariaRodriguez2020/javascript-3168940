f2300ceb9c5fdfc1151427e6d096126075136872
MariaRodriguez2020 <mariafernandarodriguez186@gmail.com>
2025-11-05 17:24:47 -0500

    Add files via upload
    base de datos

---
+++ 03_base_datos_comic/bd.js
@@ -0,0 +1,35 @@
+let comic = {
+    {
+        {
+  "nombreComic": "Aluna",
+  "numeroEpisodios": "tres",
+  "genero": ficcion,
+  "sinopsos": "Super tower",
+  "autores": maria fernanda, juan david,
+  "capitulos": [tres
+    {
+      "nombre": "el llamado del mar",
+      "personajes": [carlos vives, carlos vives niño],
+      "portada": "",
+      "descripcion": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+    {
+      "nombre": "ecos del camino",
+      "personajes": [carlos vives, carlos vives niño,]
+      "portada": "Jane Wilson",
+      "descripcion": [
+      ]
+    },
+    {
+      "nombre": "despertar",
+      "personajes": [carlos vives, carlos vives niño],
+      "portada": "Unknown",
+      "descripcion": [
+        
+      ]
+    }
+  ]
+}
+    },
+    {},
+}
